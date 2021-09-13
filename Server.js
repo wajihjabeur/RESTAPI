706d786e28a4ddee670d24f41293057dc152243f
wajihjabeur <jabeur.wajih@gmail.com>
2021-09-13 08:55:46 +0100

    commit test

--- Server.js
+++ Server.js
@@ -6,6 +6,7 @@ app.listen(5000,(err)=>{
 })
 
 require('dotenv').config({path:'./Config/.env'})
+// connect to server 
 
 mongoose.connect(process.env.MONGO_URI,
     { useNewUrlParser: true, useUnifiedTopology: true },
